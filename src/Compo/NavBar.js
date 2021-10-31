a8f46d8a6c6f3ad641a74c3f143d6fdc06ae91c7
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-10-31 14:49:32 -0500

    update-nav-backgroud-color

--- src/Compo/NavBar.js
+++ src/Compo/NavBar.js
@@ -3,11 +3,11 @@ import {Link} from 'react-router-dom'
 import styled from 'styled-components';
 import {Search, ShoppingCartOutlined} from '@material-ui/icons'
 import {Badge} from '@material-ui/core'
-import {mobile,table,all} from './Responsive'
+import {mobilex,mobile,table,all} from './Responsive'
 const Container =  styled.div`
 height: 100px;
 ${mobile(
-    {backgroundColor: 'red',justifyContent: 'space-between',height: '75px'}
+    {backgroundColor: 'grey',justifyContent: 'space-between',height: '75px'}
 )}
 }
 ${table(
--- src/Compo/Responsive.js
+++ src/Compo/Responsive.js
@@ -2,7 +2,13 @@ import {css} from 'styled-components'
 
 export const mobile = (props) => {
     return css `
-    @media only screen and (min-width:400px){
+    @media only screen and (max-width:320px){
+        ${props}
+    `
+}
+export const mobilex = (props) => {
+    return css `
+    @media only screen and (min-width:320px){
         ${props}
     `
 }
