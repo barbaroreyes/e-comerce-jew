d1fb491927ee142d05dcc3d1e43f84d83b3aa99e
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-11-01 10:54:04 -0500

    update-categories

--- src/Compo/Categories.js
+++ src/Compo/Categories.js
@@ -1,12 +1,12 @@
 import React ,{useState,useEffect} from 'react';
 import styled from 'styled-components';
-// import Amplify, {API,graphqlOperation} from 'aws-amplify'
-// import confi from '../aws-exports';
+import Amplify, {API,graphqlOperation} from 'aws-amplify'
+import confi from '../aws-exports';
 import {mobile,mobilex,table,all} from './Responsive';
-// import {listCates} from '../graphql/queries'
-// import {categorie} from './CategoriesData'
+import {listCates} from '../graphql/queries'
+import {categorie} from './CategoriesData'
 import CategorieItem from '../Compo/CategorieItem'
-// Amplify.configure(confi)
+Amplify.configure(confi)
 
 const Container = styled.div`
 display:flex;
@@ -24,26 +24,26 @@ ${all(
 const Categories = (props) => {
 
 
-  // const [categorie ,setCategorie] = useState([])
+  const [categorie ,setCategorie] = useState([])
 
-      //  useEffect(()=>{
-      //    fetchCate()
-      //    console.log('useEffect')
-      //  },[])
-      //  const fetchCate = async () => {
-      //    try {
-      //      const cateData = await API.graphql(graphqlOperation((lCatesist)))
-      //      const  catelist = cateData.data.listCates.items
-      //      setCategorie(catelist)
+       useEffect(()=>{
+         fetchCate()
+         console.log('useEffect')
+       },[])
+       const fetchCate = async () => {
+         try {
+           const cateData = await API.graphql(graphqlOperation((listCates)))
+           const  catelist = cateData.data.listCates.items
+           setCategorie(catelist)
            
-      //    } catch (error) {
+         } catch (error) {
            
-      //    }
-      //  }
+         }
+       }
        console.log('categorie',props.data)
   return (
     <Container>
-      {props.data.map((item,i)=> {
+      {categorie.map((item,i)=> {
           return(
               <CategorieItem  key={i}item={item}/>
           )
